3cf6af53e49d94c0e0f7fda023d286d48691b639
useyourillusions <minsk.bay@gmail.com>
2022-04-03 18:39:40 +0300

    Update app version to v1.1.0

--- src/middlewares/uncought-error-handler.ts
+++
@@ -1,7 +0,0 @@
-import { ErrorRequestHandler, Response, Request, NextFunction } from 'express';
-
-export const uncoughtErrorHandler: ErrorRequestHandler = async (err, req: Request, res: Response, next: NextFunction) => {
-    console.error(err.stack);
-
-    res.status(500).send(err?.message || 'Something has benn broken!');
-};
